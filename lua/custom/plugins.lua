1f6d3ee1909a753aafca75292124d3f75300051c
yasin <yascanary0@gmail.com>
2023-01-15 23:07:57 +0330

    change theme and add kj shortcut to switch to the normal mode

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,15 @@
+return function(use)
+  use 'folke/tokyonight.nvim'
+  use({
+    "lukas-reineke/indent-blankline.nvim",
+      config = function()
+        require("indent_blankline").setup({
+	    space_char_blankline = " ",
+	    show_current_context = true,
+	    show_current_context_start = true,
+	    char = '|',
+	    show_trailing_blankline_indent = false,
+	})
+      end
+  })
+end
