2ce3318def9a9ff3958d26beae81e7cace307485
iya30n <varmeziyaryasin@gmail.com>
2023-01-17 18:18:08 +0330

    set tabs size to 4 spaces

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,6 +1,11 @@
 local options = { noremap = true }
 vim.keymap.set("i", "kj", "<Esc>", options)
 
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.expandtab = true
+vim.bo.softtabstop = 4
+
 vim.o.termguicolors = true
 vim.cmd [[colorscheme ayu-mirage]]
 
