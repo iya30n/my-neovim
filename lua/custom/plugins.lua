fba663ab00d01fcdec570b9e1f5b8b083775e305
yasin <yascanary0@gmail.com>
2023-01-19 21:56:53 +0330

    install nerdtree

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,4 +2,11 @@ return function(use)
   use 'folke/tokyonight.nvim'
   use 'Shatur/neovim-ayu'
   use 'm4xshen/autoclose.nvim'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end
