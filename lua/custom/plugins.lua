fec95e8f746a87b5d91c30fa4d548dd4cf51668e
yasin <yascanary0@gmail.com>
2023-08-14 15:47:30 +0330

    install dashboard

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -16,4 +16,55 @@ return function(use)
   }
 
   use 'christoomey/vim-tmux-navigator'
+
+  use {
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper',
+        config = {
+          week_header = {
+            enable = true,
+          },
+          project = {
+            enable = true,
+          },
+          disable_move = true,
+          shortcut = {
+            {
+              desc = 'Update',
+              icon = ' ',
+              group = 'Include',
+              action = 'Lazy update',
+              key = 'u',
+            },
+            {
+              icon = ' ',
+              desc = 'Files',
+              group = 'Function',
+              action = 'Telescope find_files find_command=rg,--ignore,--hidden,--files',
+              key = 'f',
+            },
+            {
+              icon = ' ',
+              desc = 'Apps',
+              group = 'String',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              icon = ' ',
+              desc = 'dotfiles',
+              group = 'Constant',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+        },
+      }
+    end,
+
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
 end
